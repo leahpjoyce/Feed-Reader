b765c747506fdaf44f76cf0afd891d3c7efa8919
Leah Joyce <leahpjoyce@gmail.com>
2018-07-09 17:36:28 -0500

    feat: Add new test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('URL defined and that the URL is not empty', function() {
+        it('url defined and not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).not.toBe(0);
@@ -42,7 +42,7 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        it('name defined and the name is not empty', function() {
+        it('name defined and not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined();
                 expect(feed.name.length).not.toBe(0);
@@ -69,7 +69,7 @@ $(function() {
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
-        it('menu changes visibility when the menu icon is clicked', function() {
+        it('when menu icon is click changes visibility', function() {
             icon.click();
             expect(body.hasClass('menu-hidden')).toBe(false);
             icon.click();
@@ -87,13 +87,14 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
         beforeEach(function(done) {
-            loadFeed(0, done);
+            loadFeed(0, function() {
+                done();
+            });
         });
 
-        it('function completes its work', function() {
-
+        it('single entry element within the feed container', function() {
+            expect($(".feed .entry").length).not.toBe(0);
         });
-
 
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
@@ -102,5 +103,20 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var oldFeed;
+
+        beforeEach(function(done) {
+            loadFeed(1, function() {
+                oldFeed = $('.feed').html();
+                done();
+            });
+        });
+
+        it('new test is loaded', function(done) {
+            loadFeed(2, function() {
+                expect($('.feed').html()).not.toEqual(oldFeed);
+                done();
+            });
+        });
     });
 }());
